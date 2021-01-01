38b6d109ab5efeaf61825ccf51a7d3c96454b1db
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-01 14:16:00 +0100

    Use OCLint https://github.com/oclint/oclint/releases/tag/v20.11

--- do_oclint.sh
+++ do_oclint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#
+# Lint all C++ files in this folder with OCLint
+#
+# Usage:
+#
+#   ./do_oclint.sh
+#
 
-cpp_files=`ls *.cpp`
+# All C++ files
+cpp_files=$(ls *.cpp)
 
 # Clean main should work
-./oclint-0.10.3/bin/oclint -o oclint.log \
+./oclint-20.11/bin/oclint -o oclint.log \
   $cpp_files \
   -- -c -std=c++14
 
@@ -11,8 +19,10 @@ cat oclint.log
 
 # Will be 0 if success
 # Will be 1 if fail
-fail=`egrep "Compiler Errors" oclint.log | wc -l`
+fail=$(egrep "Compiler Errors" oclint.log | wc -l)
 
+# Here you can pick how to handle compile errors, where
+# one can be strict (by using 'exit 1') or mild (by removing that line)
 if [ $fail -eq 1 ]; 
 then
   echo "OCLint: Compiler error"
@@ -23,7 +33,7 @@ fi
 
 # Will be 1 if success
 # Will be 0 if fail
-success=`egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" oclint.log | wc -l`
+success=$(egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" oclint.log | wc -l)
 
 if [ $success -eq 1 ]; 
 then
--- install_oclint.sh
+++ install_oclint.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-wget https://github.com/oclint/oclint/releases/download/v0.10.3/oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz
-tar -zxf oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz
+#
+# Downloads and unzips OCLint
+#
+# Usage:
+#
+#   ./install_oclint.sh
+#
+#
+
+# Old
+#wget https://github.com/oclint/oclint/releases/download/v0.10.3/oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz
+
+# New
+wget https://github.com/oclint/oclint/releases/download/v20.11/oclint-20.11-llvm-11.0.0-x86_64-linux-ubuntu-20.04.tar.gz
+
+# Old
+#tar -zxf oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz
+
+tar -zxf $(ls *.tar.gz)
+
